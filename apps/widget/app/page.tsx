3cce2bb1f41fb597f02c7d06f9a1edaf34cf6530
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2026-04-12 02:41:48 -0400

    feat: turbo repo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,15 @@
 import { Button } from "@workspace/ui/components/button"
-
+import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project /web ready!</h1>
+          <h1 className="font-medium">Project /widget ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
-          
+          <Input />
           <Button className="mt-2">Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
