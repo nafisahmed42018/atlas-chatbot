4c16be4a201b00155001263535857ba9d56a146a
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2026-04-16 08:54:58 -0400

    feat: conversation status manual change added

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -17,9 +17,11 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        tertiary: "bg-gradient-to-b from-[#3FB62F] to-[#318d25] text-white hover:to-[#318d25]/90",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        warning: "bg-gradient-to-b from-yellow-500 to-[#be8b00] text-white hover:to-[#be8b00]/90",
         transparent: "bg-transparent text-primary-foreground hover:bg-transparent hover:text-primary-foreground/80",
       },
       size: {
