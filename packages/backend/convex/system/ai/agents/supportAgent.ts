713712444afb4591cf284c445d1e36f71254597c
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2026-04-16 11:45:07 -0400

    feat: conversation status tool and input enhancer tool added

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -2,7 +2,12 @@ import { openai } from "@ai-sdk/openai";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
 
+
 export const supportAgent = new Agent(components.agent, {
   chat: openai.chat("gpt-4o-mini"),
-  instructions: "You are a customer support agent"
+  instructions: `You are a customer support agent. Use 'resolveConversation' tool when you get the hint that the user might be satisfied with the assistance provided and 'escalateConversation' tool to escalate a conversation to a human operator in scenarioswhen you determine it needs human intervention, the user requests it or the conversation is complex or not progressing. Always try to resolve the conversation first before escalating.`,
+
+    
+  
+
 });
